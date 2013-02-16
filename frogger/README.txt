c99b7e993257cf79b93a1fca6f41a499ca4374d1
smeldrum <spencermeldrum@gmail.com>
2013-02-16 16:53:20 -0500

    Finished assignment

---
+++ frogger/README.txt
@@ -0,0 +1,5 @@
+Frogger (Part 1), by Spencer Meldrum
+
+Implementation: Everything has been implemented correctly in terms of the rendering
+Collaborations: Talked over reusability of functions/initialization with Zoe Sobin
+Hours Spent: 5 Hours
--- frogger/game.js
+++ frogger/game.js
@@ -1,7 +1,7 @@
 // Your work goes here...
 NUMLIVES = 3;
 GAMEOVER = 0;
-LEVELNUMBER = "LEVEL 1";
+LEVELNUMBER = 1;
 TIME = 0;
 SCORE=0;
 HIGHSCORE=0;
@@ -19,11 +19,14 @@ HIGHWAYWIDTH = 399;
 HIGHWAYHEIGHT = 172;
 PURPLERECTWIDTH= 399;
 PURPLERECTHEIGHT = 35;
+GAMEBOARDWIDTH=399;
+
 
 
 function start_game(){
 	var gameboard=document.getElementById('game');
 	if(gameboard.getContext){
+		initializeBoard(); //will be used if life lost as well
 		drawstaticimages(gameboard);
 		drawchangingimages(gameboard);
 		drawText(gameboard);	
@@ -37,22 +40,21 @@ function drawstaticimages(drawing){
 	ImageArea.beginPath();
 	ImageArea.rect(ORANGEBORDERWIDTH,ORANGEBORDERHEIGHT,WATERWIDTH,WATERHEIGHT);
 	ImageArea.fillStyle = "#191970";
-	ImageArea.fill();
+	ImageArea.fill(); //Water Rectangle
 	ImageArea.beginPath();
 	ImageArea.rect(ORANGEBORDERWIDTH,ORANGEBORDERHEIGHT+WATERHEIGHT+34,HIGHWAYWIDTH, HIGHWAYHEIGHT);
 	ImageArea.fillStyle = "#000000";
-	ImageArea.fill();
+	ImageArea.fill(); //Highway rectangle
 	SpriteSheet= new Image();
 	SpriteSheet.src="assets/frogger_sprites.png";
-	ImageArea.drawImage(SpriteSheet, 15, 12, 320, 30, 15, 13, 320, 30);
-	ImageArea.drawImage(SpriteSheet, 0, 55, 399, 53, 1, 59, 399, 53);
-	ImageArea.drawImage(SpriteSheet, 0, 55, 399, 53, 1, 59, 399, 53);
-	ImageArea.drawImage(SpriteSheet, 0, 118, 399, 35, 1, WATERHEIGHT, PURPLERECTWIDTH, PURPLERECTHEIGHT);
-	ImageArea.drawImage(SpriteSheet, 0, 118, 399, 35, 1, WATERHEIGHT+34+HIGHWAYHEIGHT, PURPLERECTWIDTH, PURPLERECTHEIGHT);
+	ImageArea.drawImage(SpriteSheet, 15, 12, 320, 30, 15, 13, 320, 30); //Frogger Sign
+	ImageArea.drawImage(SpriteSheet, 0, 55, GAMEBOARDWIDTH, 53, 1, 59, GAMEBOARDWIDTH, 53); //Green thing
+	ImageArea.drawImage(SpriteSheet, 0, 118, GAMEBOARDWIDTH, 35, 1, WATERHEIGHT, PURPLERECTWIDTH, PURPLERECTHEIGHT); //Purple Rectangle 1
+	ImageArea.drawImage(SpriteSheet, 0, 118, GAMEBOARDWIDTH, 35, 1, WATERHEIGHT+34+HIGHWAYHEIGHT, PURPLERECTWIDTH, PURPLERECTHEIGHT);  //Purple Rectangle 2
 	ImageArea.beginPath();
 	ImageArea.rect(ORANGEBORDERWIDTH,ORANGEBORDERHEIGHT+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT-2,HIGHWAYWIDTH, 50);
 	ImageArea.fillStyle = "#000000";
-	ImageArea.fill();
+	ImageArea.fill(); //Bottom black area
 
 }
 
@@ -60,24 +62,34 @@ function drawchangingimages(drawing){
 	ImageArea=drawing.getContext('2d');
 	SpriteSheet= new Image();
 	SpriteSheet.src="assets/frogger_sprites.png";
-	ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 1, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22);
+	//will check before this will print whether no lives, and end the game
+	ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 1, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22); //Life #1
 	if(NUMLIVES>=2){
-		ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 20, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22);
+		ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 20, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22); //Life #2
 	if(NUMLIVES==3){
-			ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 40, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22);
+			ImageArea.drawImage(SpriteSheet, 14, 335, 17, 22, 40, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3, 17, 22); //Life #3
 			}
 	}
-	ImageArea.drawImage(SpriteSheet, 14, 363, 20, 22, FROG['currentPosition']['x'], FROG['currentPosition']['y'], 17, 22);
-	ImageArea.drawImage(SpriteSheet, 8, 231, 87, 22, LOGS[0]['currentPosition']['x'], LOGS[0]['currentPosition']['y'], 87, 22);
-	ImageArea.drawImage(SpriteSheet, 8, 266, 31, 21, VEHICLES[0]['currentPosition']['x'], VEHICLES[0]['currentPosition']['y'], 31, 21);
+	ImageArea.drawImage(SpriteSheet, 14, 363, 20, 22, FROG['currentPosition']['x'], FROG['currentPosition']['y'], 17, 22); //Drawing the frog
+	ImageArea.drawImage(SpriteSheet, 8, 231, 87, 22, LOGS[0]['currentPosition']['x'], LOGS[0]['currentPosition']['y'], 87, 22); //Drawing the logs (only 1 for now)
+	ImageArea.drawImage(SpriteSheet, 8, 266, 31, 21, VEHICLES[0]['currentPosition']['x'], VEHICLES[0]['currentPosition']['y'], 31, 21); //Drawing the Vehicles (only 1 for now)
 }
 
 function drawText(drawing){
 	ImageArea=drawing.getContext('2d');
 	ImageArea.font = '20pt Calibri';
     ImageArea.fillStyle = '#7FFF00';
-    ImageArea.fillText(LEVELNUMBER, 60, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3+21);
+    ImageArea.fillText("LEVEL "+LEVELNUMBER, 60, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3+21);
     ImageArea.font='10pt Calibri';
     ImageArea.fillText("SCORE : "+SCORE, 1, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3+40);
     ImageArea.fillText("HIGHSCHORE : "+HIGHSCORE, 70, 1+WATERHEIGHT+PURPLERECTHEIGHT+HIGHWAYHEIGHT+PURPLERECTHEIGHT+3+40);
+}
+
+function initializeBoard(){
+	FROG['currentPosition']['x']= FROG['originalPosition']['x'];
+	FROG['currentPosition']['y']=FROG['originalPosition']['y'];
+	VEHICLES[0]['currentPosition']['x']=VEHICLES[0]['originalPosition']['x'];
+	VEHICLES[0]['currentPosition']['y']=VEHICLES[0]['originalPosition']['y'];
+	LOGS[0]['currentPosition']['x']=LOGS[0]['originalPosition']['x'];
+	LOGS[0]['currentPosition']['y']=LOGS[0]['originalPosition']['y'];
 }
